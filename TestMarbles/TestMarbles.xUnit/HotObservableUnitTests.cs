0a3b641f8dd1b7f2552d4a566d47a154261e280b
Paweł Bańka <pawel@pawelbanka.net>
2016-01-09 23:35:25 +0100

    Creating first unit test, wiring up logging

---
+++ TestMarbles/TestMarbles.xUnit/HotObservableUnitTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using Microsoft.Reactive.Testing;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TestMarbles.xUnit
+{
+    public class HotObservableUnitTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public HotObservableUnitTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void EmptySequenceGivesObsNever()
+        {
+            var scheduler = new TestScheduler();
+            var observable = scheduler.CreateHotObservable<int>("-");
+            observable.Subscribe(
+                o => _output.WriteLine($"{o}"),
+                ex => _output.WriteLine(ex.Message),
+                () => _output.WriteLine("End"));
+            scheduler.Start();
+            _output.WriteLine(scheduler.Now.ToString());
+        }
+    }
+}
---
+++ TestMarbles/TestMarbles/TestSchedulerExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Reactive.Testing;
+
+namespace TestMarbles
+{
+    public static class TestSchedulerExtensions
+    {
+        public static ITestableObservable<T> CreateHotObservable<T>(
+            this TestScheduler scheduler,
+            string sequence,
+            IDictionary<string, T> values = null,
+            string error = null)
+        {
+            if (scheduler == null) throw new ArgumentNullException(nameof(scheduler));
+            if (string.IsNullOrWhiteSpace(sequence)) throw new ArgumentException("Cannot be either null, empty, nor whitespace only.", nameof(sequence));
+            var input = sequence.Trim();
+            var events = new List<Recorded<Notification<T>>>();
+            return scheduler.CreateHotObservable(Enumerable.Empty<Recorded<Notification<T>>>().ToArray());
+        }
+    }
+}
