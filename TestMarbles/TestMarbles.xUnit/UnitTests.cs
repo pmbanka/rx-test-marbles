904c997beae81fc76b67c0c0719df2f2d046a89c
Paweł Bańka <pawel@pawelbanka.net>
2016-01-10 01:37:08 +0100

    Ported marbles parsing. Implemented creating hots and colds

---
+++ TestMarbles/TestMarbles.xUnit/UnitTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using Microsoft.Reactive.Testing;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TestMarbles.xUnit
+{
+    public class UnitTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public UnitTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void Hot()
+        {
+            var scheduler = new TestScheduler();
+            var observable = scheduler.CreateHotObservable<char>("      ---a---b---|");
+            observable.Subscribe(
+                o => _output.WriteLine($"{o}"),
+                ex => _output.WriteLine(ex.Message),
+                () => _output.WriteLine("End"));
+            scheduler.Start();
+            _output.WriteLine("Time " + scheduler.Now.Ticks);
+            _output.WriteLine("Cnt " + observable.Messages.Count);
+        }
+
+        [Fact]
+        public void Cold()
+        {
+            var scheduler = new TestScheduler();
+            var observable = scheduler.CreateColdObservable<char>("---#");
+            observable.Subscribe(
+                o => _output.WriteLine($"{o}"),
+                ex => _output.WriteLine(ex.Message),
+                () => _output.WriteLine("End"));
+            scheduler.Start();
+            _output.WriteLine(scheduler.Now.Ticks.ToString());
+        }
+    }
+}
--- TestMarbles/TestMarbles/TestSchedulerExtensions.cs
+++ TestMarbles/TestMarbles/TestSchedulerExtensions.cs
@@ -10,17 +10,108 @@ namespace TestMarbles
 {
     public static class TestSchedulerExtensions
     {
+        public const long FrameTimeFactor = 10;
+
+        private static IEnumerable<Recorded<Notification<T>>> parseMarbles<T>(
+            string marbles,
+            IReadOnlyDictionary<char, T> values,
+            Exception error)
+        {
+            if (marbles.IndexOf('!') != -1)
+            {
+                throw new ArgumentException("Conventional marble diagrams cannot have unsubscription marker '!'", nameof(marbles));
+            }
+            long subscribeIndex = marbles.IndexOf('^');
+            long frameOffset = subscribeIndex == -1 ? 0 : subscribeIndex * -FrameTimeFactor;
+            long groupStart = -1;
+            for (int i = 0; i < marbles.Length; i++)
+            {
+                long frame = i * FrameTimeFactor + frameOffset;
+                Notification<T> notification = null;
+                var c = marbles[i];
+                switch (c)
+                {
+                    case '-':
+                    case ' ':
+                        break; // TODO error
+                    case '(':
+                        groupStart = frame;
+                        break;
+                    case ')':
+                        groupStart = -1;
+                        break;
+                    case '|':
+                        notification = Notification.CreateOnCompleted<T>();
+                        break;
+                    case '^':
+                        break;
+                    case '#':
+                        notification = Notification.CreateOnError<T>(error ?? new Exception("error"));
+                        break;
+                    default:
+                        dynamic value = values != null ? values[c] : (dynamic)c;
+                        notification = Notification.CreateOnNext(value);
+                        break;
+                }
+                if (notification != null)
+                {
+                    yield return new Recorded<Notification<T>>(groupStart > -1 ? groupStart : frame, notification);
+                }
+            }
+        }
+
+        public static ITestableObservable<T> CreateColdObservable<T>(
+            this TestScheduler scheduler,
+            string marbles,
+            IReadOnlyDictionary<char, T> values = null,
+            Exception error = null)
+        {
+            if (scheduler == null)
+            {
+                throw new ArgumentNullException(nameof(scheduler));
+            }
+            if (string.IsNullOrWhiteSpace(marbles))
+            {
+                throw new ArgumentException("Cannot be either null, empty, nor whitespace.", nameof(marbles));
+            }
+            if (marbles.IndexOf('^') != -1)
+            {
+                throw new ArgumentException("Cold observable cannot have subscription offset '^'", nameof(marbles));
+            }
+            if (marbles.IndexOf('!') != -1)
+            {
+                throw new ArgumentException("Cold observable cannot have unsubscription marker '!'", nameof(marbles));
+            }
+            if (values == null && typeof(T) != typeof(char))
+            {
+                throw new ArgumentNullException(nameof(values), "If observable type is not char, values dictionary has to be provided");
+            }
+            var input = marbles.Trim();
+            var events = parseMarbles(input, values, error).ToArray();
+            return scheduler.CreateColdObservable(events);
+        }
+
         public static ITestableObservable<T> CreateHotObservable<T>(
             this TestScheduler scheduler,
             string sequence,
-            IDictionary<string, T> values = null,
-            string error = null)
+            IReadOnlyDictionary<char, T> values = null,
+            Exception error = null)
         {
-            if (scheduler == null) throw new ArgumentNullException(nameof(scheduler));
-            if (string.IsNullOrWhiteSpace(sequence)) throw new ArgumentException("Cannot be either null, empty, nor whitespace only.", nameof(sequence));
+            if (scheduler == null)
+            {
+                throw new ArgumentNullException(nameof(scheduler));
+            }
+            if (string.IsNullOrWhiteSpace(sequence))
+            {
+                throw new ArgumentException("Cannot be either null, empty, nor whitespace only.", nameof(sequence)); 
+            }
+            if (values == null && typeof(T) != typeof(char))
+            {
+                throw new ArgumentNullException(nameof(values), "If observable type is not char, values dictionary has to be provided");
+            }
             var input = sequence.Trim();
-            var events = new List<Recorded<Notification<T>>>();
-            return scheduler.CreateHotObservable(Enumerable.Empty<Recorded<Notification<T>>>().ToArray());
+            var events = parseMarbles(input, values, error).ToArray();
+            return scheduler.CreateHotObservable(events);
         }
     }
 }
