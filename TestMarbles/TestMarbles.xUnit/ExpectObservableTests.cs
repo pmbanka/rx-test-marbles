f2e18e74b7382f20bc727808e3118a2208f13969
Paweł Bańka <pawel@pawelbanka.net>
2016-03-18 20:07:16 +0100

    Added unit tests for ParseNotifications and ExpectObservable (some failing).

---
+++ TestMarbles/TestMarbles.xUnit/ExpectObservableTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestMarbles.xUnit
+{
+    public class ExpectObservableTests
+    {
+        [Fact]
+        public void ExpectObservable_should_work_for_observable_never()
+        {
+            var s = new MarbleScheduler();
+            s.ExpectObservable(Observable.Never<int>()).ToBe("-");
+            s.Start();
+        }
+
+        [Fact]
+        public void ExpectObservable_should_work_for_observable_empty()
+        {
+            var s = new MarbleScheduler();
+            s.ExpectObservable(Observable.Empty<int>(s.Scheduler)).ToBe("|");
+            s.Start();
+        }
+
+        [Fact]
+        public void ExpectObservable_should_work_for_observable_return()
+        {
+            var s = new MarbleScheduler();
+            s.ExpectObservable(Observable.Return('a', s.Scheduler)).ToBe("(a|)");
+            s.Start();
+        }
+
+        [Fact]
+        public void ExpectObservable_should_work_for_observable_throw()
+        {
+            var s = new MarbleScheduler();
+            var ex = new Exception();
+            s.ExpectObservable(Observable.Throw<char>(ex, s.Scheduler)).ToBe("#", error: ex);
+            s.Start();
+        }
+    }
+}
---
+++ TestMarbles/TestMarbles.xUnit/ParseNotificationsTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Reactive.Testing;
+using Xunit;
+
+namespace TestMarbles.xUnit
+{
+    public class ParseNotificationsTests : ReactiveTest
+    {
+        [Fact]
+        public void empty_is_parsed_correctly()
+        {
+            var n = new List<Recorded<Notification<char>>>
+            {  
+            };
+            var actual = TestSchedulerEx.ParseNotifications(n);
+            Assert.Equal("", actual);
+        }
+
+        [Fact]
+        public void OnNext_is_parsed_correctly()
+        {
+            var n = new List<Recorded<Notification<char>>>
+            {
+                OnNext(0, 'a')
+            };
+            var actual = TestSchedulerEx.ParseNotifications(n);
+            Assert.Equal("a", actual);
+        }
+
+        [Fact]
+        public void OnError_is_parsed_correctly()
+        {
+            var n = new List<Recorded<Notification<char>>>
+            {
+                OnError<char>(0, new Exception())
+            };
+            var actual = TestSchedulerEx.ParseNotifications(n);
+            Assert.Equal("#", actual);
+        }
+
+        [Fact]
+        public void OnCompleted_is_parsed_correctly()
+        {
+            var n = new List<Recorded<Notification<char>>>
+            {
+                OnCompleted<char>(0)
+            };
+            var actual = TestSchedulerEx.ParseNotifications(n);
+            Assert.Equal("|", actual);
+        }
+
+        [Fact]
+        public void mixed_is_parsed_correctly()
+        {
+            var n = new List<Recorded<Notification<char>>>
+            {
+                OnNext(10, 'X'),
+                OnNext(50, 'Y'),
+                OnCompleted<char>(60)
+            };
+            var actual = TestSchedulerEx.ParseNotifications(n);
+            Assert.Equal("-X---Y|", actual);
+        }
+
+        [Fact]
+        public void groups_are_parsed_correctly()
+        {
+            var n = new List<Recorded<Notification<char>>>
+            {
+                OnNext(10, 'X'),
+                OnNext(50, 'Y'),
+                OnCompleted<char>(50)
+            };
+            var actual = TestSchedulerEx.ParseNotifications(n);
+            Assert.Equal("-X---(Y|)", actual);
+        }
+    }
+}
